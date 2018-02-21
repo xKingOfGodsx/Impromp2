289f7ce6a24f8b35176de681cf1dd77e90f76a47
xKingOfGodsx <36708552+xKingOfGodsx@users.noreply.github.com>
2018-02-21 11:02:42 -0700

    Create keys.js

---
+++ keys.js
@@ -0,0 +1,12 @@
+/**** keys.js ****/
+
+module.exports = {
+    discord: 'NDE1OTE5NjExMzg5NjczNDcy.DW868w.XKK-QRPPhc6rgPMkSCciHGXfrhk',
+    mongo: {
+        url: 'localhost',
+        port: 27017,
+        db: 'database',
+        user: 'username',
+        pwd: 'password',
+    }
+};
